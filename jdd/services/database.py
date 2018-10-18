7257ad20ddd29eaaea94b9ab115084e99d667487
vueee <34796156+vueee@users.noreply.github.com>
2018-10-18 17:35:42 +0800

    Update database.py

--- jdd/services/database.py
+++ jdd/services/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-engine = create_engine('mysql://root:123qweasD@localhost/jdiandian?charset=utf8', convert_unicode=True)
+engine = create_engine('mysql://root:yourpassword@localhost/yourdb?charset=utf8', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
